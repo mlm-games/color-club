d9e9182500e31e133ddee450d99006512694f62d
mlm-studios <you@example.com>
2025-02-26 08:28:30 +0530

    Implement a proper transform analyser with transform() handling

--- scripts/svg_parser_main.gd
+++ scripts/svg_parser_main.gd
@@ -13,12 +13,12 @@ extends EditorScript
 var original_color_dict : Dictionary[StringName, Color] = {}
 
 func _run() -> void:
-	var parser = XMLParser.new()
+	var parser := XMLParser.new()
 	parser.open("res://assets/art/pic2.svg")
 	while parser.read() != ERR_FILE_EOF:
 		if parser.get_node_type() == XMLParser.NODE_ELEMENT:
-			var node_name = parser.get_node_name()
-			var attributes_dict = {}
+			var node_name := parser.get_node_name()
+			var attributes_dict := {}
 			for idx in range(parser.get_attribute_count()):
 				#TODO: if reduces code heavily, insert after typing values: if (parser.get_attribute_value(idx)).is_valid_float() : attributes_dict[parser.get_attribute_name(idx)] = float(parser.get_attribute_value(idx))
 				attributes_dict[parser.get_attribute_name(idx)] = parser.get_attribute_value(idx)
@@ -26,18 +26,19 @@ func _run() -> void:
 			match node_name:
 				"rect":
 					print("The ", node_name, " element has the following attributes: ", attributes_dict)
-					var shape = create_rect_shape(attributes_dict)
+					var shape := create_rect_shape(attributes_dict)
 					#print(create_rect_shape(attributes_dict).global_position)
 					self.get_scene().add_child(shape)
 					shape.owner = self.get_scene()
 				"circle":
 					#print(create_circle_shape(attributes_dict).global_position)
 					#self.get_scene().add_child(create_circle_shape(attributes_dict))
-					var shape = create_circle_shape(attributes_dict)
+					var shape := create_circle_shape(attributes_dict)
 					self.get_scene().add_child(shape)
 					shape.owner = self.get_scene()
 				"path":
-					var shape = create_path_shape(attributes_dict)
+					var shape := create_path_shape(attributes_dict)
+					
 					self.get_scene().add_child(shape)
 					shape.owner = self.get_scene()
 				"line":
@@ -55,7 +56,7 @@ static func create_rect_shape(attributes_dict: Dictionary) -> Panel:
 	var style_box := StyleBoxFlat.new()
 	panel.add_theme_stylebox_override("panel", style_box)
 	
-	for attribute in attributes_dict:
+	for attribute:StringName in attributes_dict:
 		match attribute:
 			"id":
 				panel.name = attributes_dict[attribute]
@@ -83,7 +84,9 @@ static func create_rect_shape(attributes_dict: Dictionary) -> Panel:
 			"fill":
 				style_box.bg_color = Color.html(attributes_dict[attribute])
 				print("Color: "); print(Color.html(attributes_dict[attribute]))
-			
+			"style":
+				var styles := analyse_style(attributes_dict[attribute])
+				apply_styles_for_shape(styles, panel)
 	return panel
 
 static func create_circle_shape(attributes_dict: Dictionary) -> SVGCircle:
@@ -94,7 +97,7 @@ static func create_circle_shape(attributes_dict: Dictionary) -> SVGCircle:
 static func create_path_shape(attributes_dict: Dictionary) -> SVGPath:
 	var path := SVGPath.new()
 	
-	for attribute in attributes_dict:
+	for attribute:StringName in attributes_dict:
 		match attribute:
 			"id":
 				path.name = attributes_dict[attribute]
@@ -121,23 +124,46 @@ static func analyse_style(style_params: String) -> Dictionary:
 	
 	return result
 
-func analyse_transform(transform_values: String):
-	var transform = Transform2D.IDENTITY
-	if transform_values.begins_with("translate"):
-		transform_values = transform_values.replace("translate", "").replacen("(", "").replacen(")", "")
-		var transform_split = transform_values.split_floats(",")
-		transform[2] = Vector2(transform_split[0], transform_split[1])
-	elif transform_values.begins_with("matrix"):
-		transform_values = transform_values.replace("matrix", "").replacen("(", "").replacen(")", "")
-		var matrix = transform_values.split_floats(",")
-		for i in 3:
-			transform[i] = Vector2(matrix[i*2], matrix[i*2+1])
-	elif transform_values.begins_with("scale"):
-		pass
-	elif transform_values.begins_with("rotate"):
-		pass
-	elif transform_values.begins_with("skewX"):
-		pass
-	elif transform_values.begins_with("skewY"):
-		pass
+#TODO: Do it indivdually per object shape by passing the shape also as an parameter
+
+
+func analyse_transform(transform_values: String, shape: Node2D) -> Transform2D:
+	var transform := Transform2D.IDENTITY
+	match transform_values:
+		transform_values when transform_values.begins_with("transform"):
+			transform_values = transform_values.lstrip("transform(").rstrip(")")
+			var transform_split := transform_values.split_floats(",")
+			if !transform_split[1]: transform_split[1] = 0 #From the mdn docs,TODO: Add a proper documention for anyone else's viewing sake in the future
+			transform[2] = Vector2(transform_split[0], transform_split[1])
+			
+		transform_values when transform_values.begins_with("scale"):
+			transform_values = transform_values.lstrip("scale(").rstrip(")")
+			var scale_split := transform_values.split_floats(",")
+			if !scale_split[2]: scale_split[2] = scale_split[1]
+			
+		transform_values when transform_values.begins_with("skewX"):
+			transform_values = transform_values.lstrip("skewX(").rstrip(")")
+			push_warning("Transform not yet implemented")
+			
+		transform_values when transform_values.begins_with("skewY"):
+			transform_values = transform_values.lstrip("skewY(").rstrip(")")
+			push_warning("Transform not yet implemented")
+			
+		transform_values when transform_values.begins_with("matrix"):
+			transform_values = transform_values.lstrip("matrix(").rstrip(")")
+			transform_values = transform_values.replace("matrix", "").replacen("(", "").replacen(")", "")
+			var matrix := transform_values.split_floats(",")
+			for i in 3:
+				transform[i] = Vector2(matrix[i*2], matrix[i*2+1])
+				
+		transform_values when transform_values.begins_with("rotate"):
+			transform_values = transform_values.lstrip("rotate(").rstrip(")")
+			var float_values := transform_values.split_floats(",")
+			if float_values[1]: shape.pivot_offset.x = float_values[1]
+			if float_values[2]: shape.pivot_offset.y = float_values[2]
+			push_warning("Transform not yet implemented")
+	
 	return transform
+
+static func apply_styles_for_shape(styles: Dictionary, shape: CanvasItem):
+	push_warning("Not yet implemented, styles")
